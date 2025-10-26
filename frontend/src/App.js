cb80dfbdc52e4889d958ca3d2065a167c594944e
charansai1432 <charansaichintala890@gmail.com>
2025-10-26 10:58:50 +0530

    modified the app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import UploadCard from './components/UploadCard';
 function App() {
@@ -8,20 +8,20 @@ function App() {
       <div className="min-h-screen bg-gradient-to-br from-blue-100 to-indigo-200 flex justify-center items-center">
       <UploadCard />
     </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header> */}
     </div>
   );
 }
